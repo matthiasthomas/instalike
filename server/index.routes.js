7e33481e3de204559b9416c11ada6e623834a875
matthias <matthiasthomaslamotte@gmail.com>
2017-08-19 17:12:28 -0400

    Adding image routes to index.routes file

--- server/index.routes.js
+++ server/index.routes.js
@@ -11,4 +11,7 @@ router.get('/', (req, res) => {
 const user_routes = require('./components/user/user.routes').router;
 router.use('/user', user_routes);
 
+const image_routes = require('./components/image/image.routes').router;
+router.use('/image', image_routes);
+
 exports.router = router;
